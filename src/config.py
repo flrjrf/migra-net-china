ffab52f1b77cf1b08dd3e2cd9839c9d97f28c4f4
Jason Fu <r.fu1@student.tue.nl>
2025-12-07 19:45:42 +0800

    cleaned data

--- src/config.py
+++ src/config.py
@@ -11,6 +11,7 @@ class BaseConfig:
     china_provinces_path = data_folder / "china_provinces.json"
     level_path = data_folder / "level.json"
     data_csv_path = data_folder / "data.csv"
+    cleaned_data_csv_path = data_folder / "cleaned_data.csv"    
     output_path: os.PathLike = PROJECT_ROOT / "output"
     SEED: int = 42
     geo_data_subset = ['hometown_lon', 'hometown_lat', 'first_lon', 'first_lat', 'current_lon', 'current_lat']
