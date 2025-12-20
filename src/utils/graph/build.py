f8ba35e8523f2fc6ca57c4692d9396b1d22290e2
Jason Fu <r.fu1@student.tue.nl>
2025-12-20 15:58:58 +0800

    collapsed edges

--- src/utils/graph/build.py
+++ src/utils/graph/build.py
@@ -135,6 +135,145 @@ def build_granular_graph(df: pd.DataFrame) -> nx.Graph:
     return G
 
 
+def build_granular_graph_collapsed(df: pd.DataFrame) -> nx.Graph:
+    """
+    Build a detailed migration graph from the DataFrame.
+    Most granular version so for each flow there is an edge with full attributes.
+    """
+    def fill_missing_nodes(G: nx.Graph, row: pd.Series):
+        # hometown to first flow
+        hometown_code = row['hometown_code']
+        first_flow_code = row['first_flow_code']
+        current_code = row['current_code']
+
+        if not G.has_node(hometown_code):
+            hometown_province_code = row['hometown_province_code']
+            hometown_city_code = row['hometown_city_code']
+            hometown_county_code = row['hometown_county_code']
+            hometown_longitude = row['hometown_lon']
+            hometown_latitude = row['hometown_lat']
+            hometown_node_attributes = NodeAttributes(
+                location_code=hometown_code,
+                province_code=hometown_province_code,
+                city_code=hometown_city_code,
+                county_code=hometown_county_code,
+                longitude=hometown_longitude,
+                latitude=hometown_latitude
+            )
+
+            G.add_node(hometown_code, attributes=hometown_node_attributes)
+
+        if not G.has_node(first_flow_code):
+            first_flow_province_code = row['first_flow_province_code']
+            first_flow_city_code = row['first_flow_city_code']
+            first_flow_county_code = row['first_flow_county_code']
+            first_flow_longitude = row['first_lon']
+            first_flow_latitude = row['first_lat']
+            first_flow_node_attributes = NodeAttributes(
+                location_code=first_flow_code,
+                province_code=first_flow_province_code,
+                city_code=first_flow_city_code,
+                county_code=first_flow_county_code,
+                longitude=first_flow_longitude,
+                latitude=first_flow_latitude
+            )
+
+            G.add_node(first_flow_code, attributes=first_flow_node_attributes)
+
+        if not G.has_node(current_code):
+            current_province_code = row['current_province_code']
+            current_city_code = row['current_city_code']
+            current_county_code = row['current_county_code']
+            current_longitude = row['current_lon']
+            current_latitude = row['current_lat']
+            current_node_attributes = NodeAttributes(
+                location_code=current_code,
+                province_code=current_province_code,
+                city_code=current_city_code,
+                county_code=current_county_code,
+                longitude=current_longitude,
+                latitude=current_latitude
+            )
+
+            G.add_node(current_code, attributes=current_node_attributes)
+
+            
+    G = nx.DiGraph()
+
+    for index , row in df.iterrows():
+        hometown_code = row['hometown_code']
+        first_flow_code = row['first_flow_code']
+        current_code = row['current_code']
+        fill_missing_nodes(G, row)
+
+        year_first_flow = row['year_first_flow']
+        month_first_flow = row['month_first_flow']
+        year_current_flow = row['year_current_flow']
+        month_current_flow = row['month_current_flow']
+        education_level = EducationLevel(row['edu_level'])
+        gender = Gender(row['gender'])
+        average_family_cost_per_month = row['average_family_cost_per_month']
+        average_family_income_per_month = row['average_family_income_per_month']
+        num_flows_total = row['num_flows_total']
+        if_stay = row['if_stay']
+        changed_household = row['if_change_household_local']
+        stay_duration_months = row['how_long_to_stay']
+
+
+        # add edges
+        edge_attributes_1 = EdgeAttributes(
+            from_node=hometown_code,
+            to_node=first_flow_code,
+            year = year_first_flow,
+            month = month_first_flow,
+            education_level=education_level,
+            gender=gender,
+            average_family_cost_per_month=average_family_cost_per_month,
+            average_family_income_per_month=average_family_income_per_month,
+            total_flows=num_flows_total,
+            stay_at_destination=if_stay,
+            changed_household=changed_household,
+            stay_duration_months=stay_duration_months,
+            flow_index=index,
+            from_node_attrs=G.nodes[hometown_code]['attributes'],
+            to_node_attrs=G.nodes[first_flow_code]['attributes']
+        )
+
+        if hometown_code != first_flow_code:
+            # checck if edge exists
+            if G.has_edge(hometown_code, first_flow_code):
+                G[hometown_code][first_flow_code]['weight'] += 1
+            else:
+                G.add_edge(hometown_code, first_flow_code, attributes= edge_attributes_1, weight=1)
+            # G.add_edge(hometown_code, first_flow_code, attributes=edge_attributes_1)
+
+        edge_attributes_2 = EdgeAttributes(
+            from_node=first_flow_code,
+            to_node=current_code,
+            year = year_current_flow,
+            month = month_current_flow,
+            education_level=education_level,
+            gender=gender,
+            average_family_cost_per_month=average_family_cost_per_month,
+            average_family_income_per_month=average_family_income_per_month,
+            total_flows=num_flows_total,
+            stay_at_destination=if_stay,
+            changed_household=changed_household,
+            stay_duration_months=stay_duration_months,
+            flow_index=index,
+            from_node_attrs=G.nodes[first_flow_code]['attributes'],
+            to_node_attrs=G.nodes[current_code]['attributes']
+        )
+
+        if first_flow_code != current_code:
+            if G.has_edge(first_flow_code, current_code):
+                G[first_flow_code][current_code]['weight'] += 1
+            else:
+                G.add_edge(first_flow_code, current_code, attributes=edge_attributes_2, weight=1)
+    return G
+
+
+
 # Build network with geographic data full data from all datapoints. Only used for plotting
 def build_geo_network(df: pd.DataFrame, start_year: int, end_year: int) -> nx.DiGraph:
     G = nx.DiGraph()
